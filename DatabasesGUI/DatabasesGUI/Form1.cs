7c8d005e3505b9f030818dc6cbcb6181559b9be1
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-05-04 13:02:56 -0400

    idk

--- DatabasesGUI/DatabasesGUI/Form1.cs
+++ DatabasesGUI/DatabasesGUI/Form1.cs
@@ -55,8 +55,6 @@ namespace DatabasesGUI
             HorseRacingDataSetTableAdapters.BetsTableAdapter betsAdapter = new HorseRacingDataSetTableAdapters.BetsTableAdapter();
             HorseRacingDataSet.BetsDataTable betsData = betsAdapter.GetData();
             dataGridView7.DataSource = betsData;
-
-
         }
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -84,6 +82,35 @@ namespace DatabasesGUI
             dataGridView6.ReadOnly = true;
             dataGridView7.ReadOnly = true;
             dataGridView8.ReadOnly = true;
+
+            //Populate grid View
+            HorseRacingDataSetTableAdapters.HorsesTableAdapter horsesAdapter = new HorseRacingDataSetTableAdapters.HorsesTableAdapter();
+            HorseRacingDataSet.HorsesDataTable horsesData = horsesAdapter.GetData();
+            dataGridView1.DataSource = horsesData;
+
+            HorseRacingDataSetTableAdapters.JockeysTableAdapter jockeysAdapter = new HorseRacingDataSetTableAdapters.JockeysTableAdapter();
+            HorseRacingDataSet.JockeysDataTable jockeysData = jockeysAdapter.GetData();
+            dataGridView2.DataSource = jockeysData;
+
+            HorseRacingDataSetTableAdapters.RacesTableAdapter racesAdapter = new HorseRacingDataSetTableAdapters.RacesTableAdapter();
+            HorseRacingDataSet.RacesDataTable racesData = racesAdapter.GetData();
+            dataGridView3.DataSource = racesData;
+
+            HorseRacingDataSetTableAdapters.TracksTableAdapter tracksAdapter = new HorseRacingDataSetTableAdapters.TracksTableAdapter();
+            HorseRacingDataSet.TracksDataTable tracksData = tracksAdapter.GetData();
+            dataGridView4.DataSource = tracksData;
+
+            HorseRacingDataSetTableAdapters.RidesTableAdapter ridesAdapter = new HorseRacingDataSetTableAdapters.RidesTableAdapter();
+            HorseRacingDataSet.RidesDataTable ridesData = ridesAdapter.GetData();
+            dataGridView5.DataSource = ridesData;
+
+            HorseRacingDataSetTableAdapters.GamblersTableAdapter gamblersAdapter = new HorseRacingDataSetTableAdapters.GamblersTableAdapter();
+            HorseRacingDataSet.GamblersDataTable gamblersData = gamblersAdapter.GetData();
+            dataGridView6.DataSource = gamblersData;
+
+            HorseRacingDataSetTableAdapters.BetsTableAdapter betsAdapter = new HorseRacingDataSetTableAdapters.BetsTableAdapter();
+            HorseRacingDataSet.BetsDataTable betsData = betsAdapter.GetData();
+            dataGridView7.DataSource = betsData;
 
             //Populate combobox options
             var dataSource = new List<Language>();
