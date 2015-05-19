dedeb0836dc8aa9dba5e1b269c77cb19b9c6e210
Tayler How <taylerhow@gmail.com>
2015-05-19 12:30:48 -0400

    Added helper function to refresh data tables

--- DatabasesGUI/DatabasesGUI/Form1.cs
+++ DatabasesGUI/DatabasesGUI/Form1.cs
@@ -32,33 +32,7 @@ namespace DatabasesGUI
         //View Data tab
         private void refreshDataButton_Click(object sender, EventArgs e)
         {
-            HorseRacingDataSetTableAdapters.HorsesTableAdapter horsesAdapter = new HorseRacingDataSetTableAdapters.HorsesTableAdapter();
-            HorseRacingDataSet.HorsesDataTable horsesData = horsesAdapter.GetData();
-            horsesTableView.DataSource = horsesData;
-
-            HorseRacingDataSetTableAdapters.JockeysTableAdapter jockeysAdapter = new HorseRacingDataSetTableAdapters.JockeysTableAdapter();
-            HorseRacingDataSet.JockeysDataTable jockeysData = jockeysAdapter.GetData();
-            jockeysTableView.DataSource = jockeysData;
-
-            HorseRacingDataSetTableAdapters.RacesTableAdapter racesAdapter = new HorseRacingDataSetTableAdapters.RacesTableAdapter();
-            HorseRacingDataSet.RacesDataTable racesData = racesAdapter.GetData();
-            racesTableView.DataSource = racesData;
-
-            HorseRacingDataSetTableAdapters.TracksTableAdapter tracksAdapter = new HorseRacingDataSetTableAdapters.TracksTableAdapter();
-            HorseRacingDataSet.TracksDataTable tracksData = tracksAdapter.GetData();
-            tracksTableView.DataSource = tracksData;
-
-            HorseRacingDataSetTableAdapters.RidesTableAdapter ridesAdapter = new HorseRacingDataSetTableAdapters.RidesTableAdapter();
-            HorseRacingDataSet.RidesDataTable ridesData = ridesAdapter.GetData();
-            ridesTableView.DataSource = ridesData;
-
-            HorseRacingDataSetTableAdapters.GamblersTableAdapter gamblersAdapter = new HorseRacingDataSetTableAdapters.GamblersTableAdapter();
-            HorseRacingDataSet.GamblersDataTable gamblersData = gamblersAdapter.GetData();
-            gamblersTableView.DataSource = gamblersData;
-
-            HorseRacingDataSetTableAdapters.BetsTableAdapter betsAdapter = new HorseRacingDataSetTableAdapters.BetsTableAdapter();
-            HorseRacingDataSet.BetsDataTable betsData = betsAdapter.GetData();
-            betsTableView.DataSource = betsData;
+            refreshDataTables();
         }
 
         private void runStoredProcedureButton_Click(object sender, EventArgs e)
@@ -652,6 +626,7 @@ namespace DatabasesGUI
                     }
                     break;
             }
+            refreshDataTables();
         }
 
         //Update Data tab
@@ -1289,6 +1264,37 @@ namespace DatabasesGUI
                     Console.WriteLine(exception.Number);
                     break;
             }
+        }
+
+        private void refreshDataTables()
+        {
+            HorseRacingDataSetTableAdapters.HorsesTableAdapter horsesAdapter = new HorseRacingDataSetTableAdapters.HorsesTableAdapter();
+            HorseRacingDataSet.HorsesDataTable horsesData = horsesAdapter.GetData();
+            horsesTableView.DataSource = horsesData;
+
+            HorseRacingDataSetTableAdapters.JockeysTableAdapter jockeysAdapter = new HorseRacingDataSetTableAdapters.JockeysTableAdapter();
+            HorseRacingDataSet.JockeysDataTable jockeysData = jockeysAdapter.GetData();
+            jockeysTableView.DataSource = jockeysData;
+
+            HorseRacingDataSetTableAdapters.RacesTableAdapter racesAdapter = new HorseRacingDataSetTableAdapters.RacesTableAdapter();
+            HorseRacingDataSet.RacesDataTable racesData = racesAdapter.GetData();
+            racesTableView.DataSource = racesData;
+
+            HorseRacingDataSetTableAdapters.TracksTableAdapter tracksAdapter = new HorseRacingDataSetTableAdapters.TracksTableAdapter();
+            HorseRacingDataSet.TracksDataTable tracksData = tracksAdapter.GetData();
+            tracksTableView.DataSource = tracksData;
+
+            HorseRacingDataSetTableAdapters.RidesTableAdapter ridesAdapter = new HorseRacingDataSetTableAdapters.RidesTableAdapter();
+            HorseRacingDataSet.RidesDataTable ridesData = ridesAdapter.GetData();
+            ridesTableView.DataSource = ridesData;
+
+            HorseRacingDataSetTableAdapters.GamblersTableAdapter gamblersAdapter = new HorseRacingDataSetTableAdapters.GamblersTableAdapter();
+            HorseRacingDataSet.GamblersDataTable gamblersData = gamblersAdapter.GetData();
+            gamblersTableView.DataSource = gamblersData;
+
+            HorseRacingDataSetTableAdapters.BetsTableAdapter betsAdapter = new HorseRacingDataSetTableAdapters.BetsTableAdapter();
+            HorseRacingDataSet.BetsDataTable betsData = betsAdapter.GetData();
+            betsTableView.DataSource = betsData;
         }
     }
 
